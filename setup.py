edb38127a0cebde9c1e094b029c1336d9ad74d8e
Phil Barker <phil.barker@pjjk.co.uk>
2022-09-23 16:27:42 +0100

    initial commit, all empty files

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="RDFTranslate",
+    version="0.0.0",
+    description="Translate between RDF serialization formats.",
+    author="Phil Barker",
+    author_email="phil.barker@pjjk.co.uk",
+    url="https://github.com/philbarker/TAP2SHACL",
+    packages=["rdftranslate"],
+)
