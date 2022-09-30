cd70b3ac7ee3ed1bb601d0c95337685efdf050fa
Phil Barker <phil.barker@pjjk.co.uk>
2022-09-30 15:05:15 +0100

    better way to deal with multiple expressions of same format

--- rdftranslate/translator.py
+++ rdftranslate/translator.py
@@ -1,5 +1,13 @@
 from rdflib import Graph
+from pprint import pprint
 
+knownFormats = {
+    "ttl"    : "turtle",
+    "jsonld" : "json-ld",
+    "json"   : "json-ld",
+    "n3"     : "n3",
+    "nt"     : "nt"
+}
 
 class RDFTranslator:
     """Methods to read a file containing a serialization of an RDF graphin one format and display or save it in another format."""
@@ -10,7 +18,7 @@ class RDFTranslator:
         self.g = Graph()
         self.inFileName = inFileName
         self.outFileName = outFileName
-        self.knownFormats = ["ttl", "jsonld", "json", "n3"]
+        self.knownFormats = knownFormats
         self._set_inFileFormat(inFileFormat)
         self._set_outFileFormat(outFileFormat)
 
@@ -18,15 +26,15 @@ class RDFTranslator:
         """Set the input file format to the parameter passed, or guess from the file name extension. Raise error if not a supported format."""
         if inFileFormat is not None:
             iff = inFileFormat.lower()
-            if iff in self.knownFormats:
-                self.inFileFormat = iff
+            if iff in self.knownFormats.keys():
+                self.inFileFormat = self.knownFormats[iff]
             else:
                 msg = f"File format {inFileFormat} does not correspond to a known serialization format."
                 raise ValueError(msg)
         elif "." in self.inFileName:
             ext = self.inFileName.split(".")[1].lower()
-            if ext in self.knownFormats:
-                self.inFileFormat = ext
+            if ext in self.knownFormats.keys():
+                self.inFileFormat = self.knownFormats[ext]
             else:
                 msg = f"File extension {ext} does not correspond to a known serialization format."
                 raise ValueError(msg)
@@ -38,17 +46,36 @@ class RDFTranslator:
         """Set the input file format to the parameter passed, or guess from the file name extension. Raise error if not a supported format."""
         if outFileFormat is not None:
             off = outFileFormat.lower()
-            if off in self.knownFormats:
-                self.outFileFormat = off
+            if off in self.knownFormats.keys():
+                self.outFileFormat = self.knownFormats[off]
             else:
                 msg = f"File format {outFileFormat} does not correspond to a known serialization format."
                 raise ValueError(msg)
         elif self.outFileName is not None and "." in self.outFileName:
             ext = self.outFileName.split(".")[1].lower()
-            if ext in self.knownFormats:
-                self.outFileFormat = ext
+            if ext in self.knownFormats.keys():
+                self.outFileFormat = self.knownFormats[ext]
             else:
                 msg = f"File extension {ext} does not correspond to a known serialization format."
                 raise ValueError(msg)
         else:
-            self.outFileFormat = "ttl"
+            self.outFileFormat = "turtle"
+        return
+
+    def read_graph(self):
+        """Read input file, store as graph."""
+        # yeah, maybe there could be more checks here.
+        self.g.parse(self.inFileName, format=self.inFileFormat)
+        return
+
+    def write_graph(self):
+        """Write graph to output file or console."""
+        if len(self.g) == 0:
+            msg = "Trying to write graph when no graph is stored."
+            raise ValueError(msg)
+        # yeah, maybe there could be more checks here.
+        if self.outFileName is not None:
+            self.g.serialize(destination=self.outFileName, format=self.outFileFormat)
+        else:
+            pprint(self.g.serialize(format=self.outFileFormat))
+        return
